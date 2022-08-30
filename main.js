bf2be387c2d7d75b804d18fa72ddd6b2cce56dc7
YevhDr <evgesha.drozdova@gmail.com>
2022-08-31 01:50:40 +0300

    init

--- main.js
+++ main.js
@@ -4,29 +4,71 @@ var container = document.querySelector('#scroll');
     var step = document.querySelector('#scroll > .scroll__text > .step'); // text.selectAll('.step');
     var scroller = scrollama();
 
-/*     let stepH = Math.floor(window.innerHeight * 0.9);
-    $(".step").css('height', stepH + 'px'); */
-
 function handleStepEnter(r) { 
-    let preudoClass = $(r.element).data("class");
-    console.log(preudoClass)
-    console.log(preudoClass)
+    let preudoClass = $(r.element).data("class");  
+    console.log(r.index);
+    console.log(preudoClass);
+
 
     if(preudoClass === 'show-first-pic' && r.direction ==="down"){
-        let src= $(r.element).data("src");
-        $("#pic_1").css("opacity", 0).attr("src", src).fadeTo(1500, 1 );  
+        let src = $(r.element).data("src");
+        
+        d3.selectAll(".pic")
+            .style("left", 0)
+            .style("top", 0)
+            .style("width", '100%') 
+
+        d3.select("#pic_2")
+            .style("opacity", 0);  
+        
+        d3.select("#pic_1")
+            .style("opacity", 0)
+            .attr("src", src)
+            .transition()
+            .duration(1000)
+            .style("opacity", 1)
 
     } else if(preudoClass === 'show-second-pic' && r.direction ==="down"){
         let src= $(r.element).data("src");
-        $("#pic_1").fadeTo(1500, 0 );
-        $("#pic_2").attr("src", src).css("opacity", 0)
-        $("#pic_2").fadeTo(1500, 1 ); 
+
+        d3.select("#pic_1")        
+            .transition()
+            .duration(1000)
+            .attr("opacity", 0)
+       
+        d3.select("#pic_2")
+            .style("opacity", 0)
+            .attr("src", src)
+            .transition()
+            .duration(1000)
+            .style("opacity", 1)
 
     } else if(preudoClass === 'split' && r.direction ==="down"){
-        let marginLeft = $(r.element).data("left");
-        $(".pic").css("opacity", 1);
-        $("#pic_1").animate({left:marginLeft, width:'80%', "opacity": 1}, 1500);
-        $("#pic_2").animate({left:"40%", width:'80%'}, 1500);
+        let marginLeft_1 = $(r.element).data("left-f") ?? "-15%";
+        let marginLeft_2 = $(r.element).data("left-s") ?? "40%";
+        let marginTop_1 = $(r.element).data("top-f") ?? "0";
+        let marginTop_2 = $(r.element).data("top-s") ?? '0';
+        let picWidth = '80%';
+        
+        d3.select("#pic_1")
+            .style("opacity", 1)
+            .transition()
+            .duration(1000)
+            .style("left", marginLeft_1)
+            .style("top", marginTop_1)
+            .style("width", picWidth)            
+            .on("end", function(){
+                let src= $(r.element).data("src");           
+                d3.select(this).attr("src", src);
+            })
+
+        d3.select("#pic_2")           
+            .transition()
+            .duration(1000)
+            .style("left", marginLeft_2)
+            .style("top", marginTop_2)
+            .style("width", picWidth)
+                
 
     } else if(preudoClass === 'show-red-lines' && r.direction ==="down"){
         let src= $(r.element).data("src");
@@ -35,70 +77,17 @@ function handleStepEnter(r) {
     } else if(preudoClass === 'empty-slide' && r.direction ==="down"){
         let city= $(r.element).data("city");
         $("#city-marker").text(city)
-        $(".pic").animate({left:"0", width:'100%', "opacity": 0}, 1500);      
-    }
-    
-  /*  console.log(r.index) 
-   if (r.index === 0 && r.direction === "up") {
-        $(".pic").fadeTo(1500, 0);   
-   }  else if (r.index === 1 && r.direction === "down") {
-        let src= $(r.element).data("src");
-        $("#pic_1").css("opacity", 0).attr("src", src).fadeTo(1500, 1 );    
 
-   } else if (r.index === 1 && r.direction === "up") {
-        let src= $(r.element).data("src");       
-        $("#pic_1").fadeTo(1500, 1 );  
-        $("#pic_2").fadeTo(1500, 0 );   
-
-   }
-    else if(r.index === 2 && r.direction === "down"){
-        let src= $(r.element).data("src");
-        $("#pic_1").fadeTo(1500, 0 );
-        $("#pic_2").attr("src", src).css("opacity", 0)
-        $("#pic_2").fadeTo(1500, 1 );      
-   } else if(r.index === 2 && r.direction === "up"){
-        $("#pic_1").animate({left:"0", width:'100%', "opacity": 0}, 1500);
-        $("#pic_2").animate({left:"0", width:'100%'}, 1500);
-   }
-   else if(r.index === 3 && r.direction === "down"){
-        $(".pic").css("opacity", 1);
-        $("#pic_1").animate({left:"-20%", width:'80%', "opacity": 1}, 1500);
-        $("#pic_2").animate({left:"40%", width:'80%'}, 1500);
-      
-   } else if(r.index === 3 && r.direction === "up") {
-        $(".pic").fadeTo(1500, 1);   
-       
-    } else if(r.index === 4 && r.direction === "down") {
-        let src= $(r.element).data("src");
-        $("#pic_1").attr("src", src); 
-    } else if(r.index === 5 && r.direction === "down") {
-        $(".pic").animate({left:"0", width:'100%', "opacity": 0}, 1500);      
-        
-       
-    } else if(r.index === 6 && r.direction === "down") {
-        let src= $(r.element).data("src");
-        $("#pic_1").attr("src", src).fadeTo(1500, 1 );       
-    } else if(r.index === 7 && r.direction === "down") {
-        let src= $(r.element).data("src");
-        $("#pic_1").fadeTo(1500, 0 );
-        $("#pic_2").attr("src", src).fadeTo(1500, 1 );       
-    } else if(r.index === 8 && r.direction === "down") {
-        $(".pic").css("opacity", 1);
-        $("#pic_1").animate({left:"-15%", width:'80%', "opacity": 1}, 1500);
-        $("#pic_2").animate({left:"40%", width:'80%',}, 1500); 
-    } else if(r.index === 9 && r.direction === "down") {
-        let src= $(r.element).data("src");
-        $("#pic_1").attr("src", src); 
-    }  */
+        d3.selectAll(".pic")        
+            .transition()
+            .duration(1000)
+            .style("opacity", 0)            
+    } 
 
 }
 
-  // generic window resize listener event
+
   function handleResize() {
-        // 1. update height of step elements
-        let stepH = Math.floor(window.innerHeight * 1);
-        $(step).css('height', stepH + 'px');
-        
         scroller.resize();
       }
 
@@ -111,8 +100,8 @@ function init() {
         graphic: '.scroll__graphic',
         text: '.scroll__text',
         step: '.scroll__text .step',
-        offset: 0.5,
-        debug: false
+        offset: 0.8,
+        debug: true
     })
         .onStepEnter(handleStepEnter);
 
